899e19d7dae2ba31dc28b6a6d413cc11fe355253
Sibi Sankar <sibis@codeaurora.org>
2019-07-24 16:17:27 +0530

    dt-bindings: interconnect: Add OSM L3 DT bindings
    Add bindings for Operation State Manager (OSM) L3 interconnect provider
    on SDM845 SoCs.
    
    Signed-off-by: Sibi Sankar <sibis@codeaurora.org>

---
+++ include/dt-bindings/interconnect/qcom,osm-l3.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2019 The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __DT_BINDINGS_INTERCONNECT_QCOM_OSM_L3_H
+#define __DT_BINDINGS_INTERCONNECT_QCOM_OSM_L3_H
+
+#define MASTER_OSM_L3_APPS	0
+#define MASTER_OSM_L3_GPU	1
+#define SLAVE_OSM_L3		2
+
+#endif
